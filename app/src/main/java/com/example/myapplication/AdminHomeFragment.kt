88e4273df59efc7c30c4ad6b9d89221e405532d0
dahighsky <aakashuttkarsh29@gmail.com>
2022-01-10 13:07:00 +0530

    Log In functional with "dahighsky"

---
+++ app/src/main/java/com/example/myapplication/AdminHomeFragment.kt
@@ -0,0 +1,46 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.myapplication.databinding.FragmentAdminHomeBinding
+
+
+class AdminHomeFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentAdminHomeBinding>(
+            inflater,
+            R.layout.fragment_admin_home,
+            container,
+            false
+        )
+
+        binding.logOutButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_adminHomeFragment_to_homeFragment))
+
+        binding.logButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_adminHomeFragment_to_viewLogFragment))
+
+        binding.cctvButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_adminHomeFragment_to_cctvFragment))
+
+        binding.editLogButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_adminHomeFragment_to_cctvFragment))
+
+
+
+        return binding.root
+    }
+
+
+}
--- app/src/main/java/com/example/myapplication/ui/login/AdminLoginFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/login/AdminLoginFragment.kt
@@ -15,117 +15,44 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.myapplication.databinding.FragmentAdminLoginBinding
 
 import com.example.myapplication.R
 
 class AdminLoginFragment : Fragment() {
 
-    private lateinit var loginViewModel: LoginViewModel
-    private var _binding: FragmentAdminLoginBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentAdminLoginBinding.inflate(inflater, container, false)
-        return binding.root
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
-
-        val usernameEditText = binding.username
-        val passwordEditText = binding.password
-        val loginButton = binding.login
-        val loadingProgressBar = binding.loading
-
-        loginViewModel.loginFormState.observe(viewLifecycleOwner,
-            Observer { loginFormState ->
-                if (loginFormState == null) {
-                    return@Observer
-                }
-                loginButton.isEnabled = loginFormState.isDataValid
-                loginFormState.usernameError?.let {
-                    usernameEditText.error = getString(it)
-                }
-                loginFormState.passwordError?.let {
-                    passwordEditText.error = getString(it)
-                }
-            })
-
-        loginViewModel.loginResult.observe(viewLifecycleOwner,
-            Observer { loginResult ->
-                loginResult ?: return@Observer
-                loadingProgressBar.visibility = View.GONE
-                loginResult.error?.let {
-                    showLoginFailed(it)
-                }
-                loginResult.success?.let {
-                    updateUiWithUser(it)
-                }
-            })
-
-        val afterTextChangedListener = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-                // ignore
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                // ignore
-            }
-
-            override fun afterTextChanged(s: Editable) {
-                loginViewModel.loginDataChanged(
-                    usernameEditText.text.toString(),
-                    passwordEditText.text.toString()
-                )
-            }
-        }
-        usernameEditText.addTextChangedListener(afterTextChangedListener)
-        passwordEditText.addTextChangedListener(afterTextChangedListener)
-        passwordEditText.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                loginViewModel.login(
-                    usernameEditText.text.toString(),
-                    passwordEditText.text.toString()
-                )
-            }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentAdminLoginBinding>(
+            inflater,
+            R.layout.fragment_admin_login,
+            container,
             false
-        }
+        )
 
-        loginButton.setOnClickListener {
-            loadingProgressBar.visibility = View.VISIBLE
-            loginViewModel.login(
-                usernameEditText.text.toString(),
-                passwordEditText.text.toString()
-            )
-        }
-    }
+        binding.login.setOnClickListener@Suppress("UNUSED_ANONYMOUS_PARAMETER"){
+                if (binding.username.text.toString().equals("dahighsky")
+                    &&
+                    binding.password.text.toString().equals("dahighsky")
+                ){
+                    view?.findNavController()?.navigate(R.id.action_adminLoginFragment_to_adminHomeFragment)
+                }
+                else{
+                    val text = "Invalid User details"
+                    val duration = Toast.LENGTH_SHORT
 
-    private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome) + model.displayName
-        // TODO : initiate successful logged in experience
-        val appContext = context?.applicationContext ?: return
-        Toast.makeText(appContext, welcome, Toast.LENGTH_LONG).show()
-    }
+                    val toast = Toast.makeText(context?.applicationContext, text, duration)
+                    toast.show()
 
-    private fun showLoginFailed(@StringRes errorString: Int) {
-        val appContext = context?.applicationContext ?: return
-        Toast.makeText(appContext, errorString, Toast.LENGTH_LONG).show()
-    }
+                }
+            }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+
+
+        return binding.root
     }
 }
